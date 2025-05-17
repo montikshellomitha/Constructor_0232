f171135cf9d4dee42138afc7414b5773a06c0e1e
montikshellomitha <monshelmit@gmail.com>
2025-05-17 14:34:57 +0700

    Menambahkan komentar pemanggilan method biasa

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -20,4 +20,4 @@ public:
 int main()
 {
     // bukunya.setJudul("Matematika");
-    // cout << bukunya.setJudul("Matematika").getJudul(); // chain function calls
+    // cout << bukunya.getJudul();
