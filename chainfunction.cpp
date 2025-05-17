a0af347c8a0480ce8c11987c975b607c5df45ec3
montikshellomitha <monshelmit@gmail.com>
2025-05-17 14:24:20 +0700

    Menambahkan atribut dan method setJudul

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 class buku
 {
+    string judul;
 
-    
+public:
+    buku setJudul(string judul)
+    {
+        this->judul = judul;
+        return *this; // chain function
+    }
